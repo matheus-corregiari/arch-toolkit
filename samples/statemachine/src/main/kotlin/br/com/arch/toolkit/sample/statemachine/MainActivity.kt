15e8723a2d09179acc98db1d8a8a191423d7a6b0
Matheus Corregiari <metheus.92@gmail.com>
2020-06-19 18:15:33 -0300

    Add possibility to get query parameters from extraProvider

--- samples/statemachine/src/main/kotlin/br/com/arch/toolkit/sample/statemachine/MainActivity.kt
+++ samples/statemachine/src/main/kotlin/br/com/arch/toolkit/sample/statemachine/MainActivity.kt
@@ -2,8 +2,11 @@ package br.com.arch.toolkit.sample.statemachine
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.com.arch.toolkit.delegate.ExtraType
+import br.com.arch.toolkit.delegate.extraProvider
 import br.com.arch.toolkit.delegate.viewProvider
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +14,14 @@ class MainActivity : AppCompatActivity() {
     private val viewStateMachineExampleButton: Button by viewProvider(R.id.bt_view_state_machine_example)
     private val sceneStateMachineExampleButton: Button by viewProvider(R.id.bt_scene_state_machine_example)
 
+    private val extra: String by extraProvider("teste", default = "padraozaoo", type = ExtraType.QUERY)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewStateMachineExampleButton.setOnClickListener {
+            Toast.makeText(this, "Test argument: $extra", Toast.LENGTH_SHORT).show()
             startActivity(Intent(this, ViewStateMachineExampleActivity::class.java))
         }
 
