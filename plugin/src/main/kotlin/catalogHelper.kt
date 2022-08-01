e8d2b69942aadce6cdc96161ac533406b04ad31f
Matheus Barbieri Corregiari <metheus.92@gmail.com>
2022-08-01 10:38:20 -0300

    HIHIHIIH
    maldaaade

---
+++ plugin/src/main/kotlin/catalogHelper.kt
@@ -0,0 +1,8 @@
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+
+internal const val COMPOSE_CATALOG_NAME = "compose"
+
+internal fun Project.getCatalog() = extensions.getByType<VersionCatalogsExtension>()
+    .named("libs")
