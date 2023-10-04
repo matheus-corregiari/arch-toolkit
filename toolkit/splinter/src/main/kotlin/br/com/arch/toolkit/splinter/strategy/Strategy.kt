52de73c6423205dcceddb182d0eddbccceb45f81
Matheus Barbieri Corregiari <metheus.92@gmail.com>
2023-10-04 11:31:25 -0300

    breaking everything, but it still do not working =(

--- toolkit/splinter/src/main/kotlin/br/com/arch/toolkit/splinter/strategy/Strategy.kt
+++ toolkit/splinter/src/main/kotlin/br/com/arch/toolkit/splinter/strategy/Strategy.kt
@@ -6,13 +6,25 @@ import br.com.arch.toolkit.splinter.Splinter
 import br.com.arch.toolkit.splinter.extension.emitError
 import kotlinx.coroutines.flow.FlowCollector
 
+/**
+ * Main class that defines how the Splinter will work to emit the events!
+ */
 sealed class Strategy<RESULT : Any> {
+
+    /**
+     * Here, all the magic happens
+     *
+     * This method will implement how the job inside splinter should run
+     */
     @WorkerThread
     internal abstract suspend fun execute(
         collector: FlowCollector<DataResult<RESULT>>,
         executor: Splinter<RESULT>
     )
 
+    /**
+     * In case of error inside the job, some uncaught exception on flow, this method will trigger inside splinter
+     */
     @WorkerThread
     internal open suspend fun flowError(
         error: Throwable,
@@ -20,6 +32,9 @@ sealed class Strategy<RESULT : Any> {
         executor: Splinter<RESULT>
     ) = collector.emitError(error)
 
+    /**
+     * In case of major error inside the job, some uncaught exception on job, this method will trigger inside splinter
+     */
     @WorkerThread
     internal open suspend fun majorError(
         error: Throwable,
@@ -28,6 +43,14 @@ sealed class Strategy<RESULT : Any> {
     ) = flowError(error, collector, executor)
 
     companion object {
+
+        /**
+         * Helper method that creates a OneShot strategy and configure it
+         *
+         * @param config - Block that will configure the oneShot strategy
+         *
+         * @return br.com.arch.toolkit.splinter.strategy.OneShot
+         */
         fun <T : Any> oneShot(config: OneShot<T>.Config.() -> Unit) = OneShot<T>().config(config)
     }
 }
