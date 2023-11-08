54385aa387ca9eed62253ba0d5dea0234746af28
Matheus Barbieri Corregiari <metheus.92@gmail.com>
2023-11-08 17:25:45 -0300

    Update tag.sh

--- .github/scripts/tag.sh
+++ .github/scripts/tag.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -eu
-
 output="../git-tag"
 message=$(git show -s --format=%B)
 parents=$(git show -s --format=%P | wc -w)
@@ -14,15 +12,15 @@ if [ "$parents" -ne 2 ]; then
 fi
 
 # Verify if the commit is a merge following the pattern release/0.0.0 or hotfix/0.0.0
-matched_message=$(grep -Eo 'from (release|hotfix)/(\d+.\d+.\d+) to master' <<< "$message" || true)
+matched_message=$(grep -E '(release|hotfix)\/([0-9]+.[0-9]+.[0-9]+)' <<< "$message" || true)
 if [ -z "$matched_message" ]; then
     echo -e "Are you sure that you created a proper branch name to merge with master?"
+    echo "$message"
     exit 1
 fi
 
 # Get version in message
-version=$(awk '{ print $2 }' <<< "$matched_message" | cut -d/ -f2 | tee "$output/version")
-echo -e "Release $version\n" > "$output/message"
+version=$(sed -E 's?.+(release|hotfix)/([0-9]+.[0-9]+.[0-9]+)?\2?g' <<< "$matched_message")
 echo "Tag: $version"
 
 # Create and push tag
