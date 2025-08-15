ed9b99b2967b20fa3e99f3a0e680cbb45adeeaa7
Matheus Barbieri Corregiari <metheus.92@gmail.com>
2025-08-14 23:29:29 -0300

    Refactor(EventObserver): Introduce `valueOrNull` Extension for Flow
    This commit adds a new extension function `valueOrNull()` to the `Flow` interface within the EventObserver module. This function provides a way to synchronously access the last emitted value of a `StateFlow` or the last value in the `replayCache` of a `SharedFlow`.
    
    **Key Changes:**
    
    - **`toolkit/multi/event-observer/src/commonMain/kotlin/br/com/arch/toolkit/util/_flow.kt`:**
        - Added the `valueOrNull()` extension function:
            - For a `StateFlow<T>`, it returns the current `value`.
            - For a `SharedFlow<T>`, it returns the `replayCache.lastOrNull()`.
            - For other types of `Flow`, it returns `null` as synchronous access to the last value is not generally possible.
    
    This utility function simplifies scenarios where immediate access to the latest known value of specific flow types is needed, without requiring collection.

--- toolkit/multi/event-observer/src/commonMain/kotlin/br/com/arch/toolkit/util/_flow.kt
+++ toolkit/multi/event-observer/src/commonMain/kotlin/br/com/arch/toolkit/util/_flow.kt
@@ -3,10 +3,18 @@ package br.com.arch.toolkit.util
 import br.com.arch.toolkit.result.DataResult
 import br.com.arch.toolkit.result.ObserveWrapper
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 
 suspend fun <T> Flow<DataResult<T>>.unwrap(
     config: ObserveWrapper<T>.() -> Unit
 ) {
     val wrapper = ObserveWrapper<T>().apply(config)
     collect { wrapper.suspendFunc { handleResult(it) } }
+}
+
+fun <T> Flow<T>.valueOrNull(): T? = when (this) {
+    is StateFlow<T> -> value
+    is SharedFlow<T> -> replayCache.lastOrNull()
+    else -> null
 }
